6ee64ba68139057388db16ad501b9d75c5212aaf
Aziz Light <aziz@azizlight.me>
2013-05-19 12:38:29 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Message Learn
+=============
+
+A learning app
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messagelearn::Application.config.secret_token = '93746e7e45ad2dd0191c7752e87e6011ffbc5b8550eb329b3f118395ac597fd01bba58212de477a00c4da6fa351583309e7a7f5bf2e8421dd26085af20153cf1'
